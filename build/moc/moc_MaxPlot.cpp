3f32897ae7b823a5d60c2bbd042b85f4d38b8d58
小子 <136250709+GaryAacm@users.noreply.github.com>
2025-02-24 10:51:02 +0800

    Add files via upload

---
+++ build/moc/moc_MaxPlot.cpp
@@ -0,0 +1,320 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'MaxPlot.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.3)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../MaxPlot.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'MaxPlot.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.15.3. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MaxPlot_t {
+    QByteArrayData data[31];
+    char stringdata0[430];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MaxPlot_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MaxPlot_t qt_meta_stringdata_MaxPlot = {
+    {
+QT_MOC_LITERAL(0, 0, 7), // "MaxPlot"
+QT_MOC_LITERAL(1, 8, 12), // "windowClosed"
+QT_MOC_LITERAL(2, 21, 0), // ""
+QT_MOC_LITERAL(3, 22, 15), // "sendMQTTMessage"
+QT_MOC_LITERAL(4, 38, 7), // "message"
+QT_MOC_LITERAL(5, 46, 10), // "Finish_ALL"
+QT_MOC_LITERAL(6, 57, 10), // "finishRead"
+QT_MOC_LITERAL(7, 68, 11), // "Finish_Mqtt"
+QT_MOC_LITERAL(8, 80, 11), // "Finish_Plot"
+QT_MOC_LITERAL(9, 92, 10), // "updatePlot"
+QT_MOC_LITERAL(10, 103, 13), // "onSliderMoved"
+QT_MOC_LITERAL(11, 117, 8), // "position"
+QT_MOC_LITERAL(12, 126, 19), // "onExitButtonClicked"
+QT_MOC_LITERAL(13, 146, 20), // "onStartButtonClicked"
+QT_MOC_LITERAL(14, 167, 16), // "Read_Data_Thread"
+QT_MOC_LITERAL(15, 184, 18), // "Update_Plot_Thread"
+QT_MOC_LITERAL(16, 203, 11), // "Mqtt_Thread"
+QT_MOC_LITERAL(17, 215, 16), // "Get_Mqtt_Message"
+QT_MOC_LITERAL(18, 232, 15), // "onSliderPressed"
+QT_MOC_LITERAL(19, 248, 16), // "onSliderReleased"
+QT_MOC_LITERAL(20, 265, 17), // "Http_Worker_Start"
+QT_MOC_LITERAL(21, 283, 17), // "startTimerIfReady"
+QT_MOC_LITERAL(22, 301, 15), // "stop_Plot_Timer"
+QT_MOC_LITERAL(23, 317, 16), // "stop_Mqtt_Thread"
+QT_MOC_LITERAL(24, 334, 16), // "stop_Read_Thread"
+QT_MOC_LITERAL(25, 351, 11), // "setup_timer"
+QT_MOC_LITERAL(26, 363, 11), // "start_timer"
+QT_MOC_LITERAL(27, 375, 11), // "pause_timer"
+QT_MOC_LITERAL(28, 387, 14), // "onMqttFinished"
+QT_MOC_LITERAL(29, 402, 12), // "delete_timer"
+QT_MOC_LITERAL(30, 415, 14) // "onPlotFinished"
+
+    },
+    "MaxPlot\0windowClosed\0\0sendMQTTMessage\0"
+    "message\0Finish_ALL\0finishRead\0Finish_Mqtt\0"
+    "Finish_Plot\0updatePlot\0onSliderMoved\0"
+    "position\0onExitButtonClicked\0"
+    "onStartButtonClicked\0Read_Data_Thread\0"
+    "Update_Plot_Thread\0Mqtt_Thread\0"
+    "Get_Mqtt_Message\0onSliderPressed\0"
+    "onSliderReleased\0Http_Worker_Start\0"
+    "startTimerIfReady\0stop_Plot_Timer\0"
+    "stop_Mqtt_Thread\0stop_Read_Thread\0"
+    "setup_timer\0start_timer\0pause_timer\0"
+    "onMqttFinished\0delete_timer\0onPlotFinished"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MaxPlot[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      27,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       6,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    0,  149,    2, 0x06 /* Public */,
+       3,    1,  150,    2, 0x06 /* Public */,
+       5,    0,  153,    2, 0x06 /* Public */,
+       6,    0,  154,    2, 0x06 /* Public */,
+       7,    0,  155,    2, 0x06 /* Public */,
+       8,    0,  156,    2, 0x06 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags
+       9,    0,  157,    2, 0x08 /* Private */,
+      10,    1,  158,    2, 0x08 /* Private */,
+      12,    0,  161,    2, 0x08 /* Private */,
+      13,    0,  162,    2, 0x08 /* Private */,
+      14,    0,  163,    2, 0x08 /* Private */,
+      15,    0,  164,    2, 0x08 /* Private */,
+      16,    0,  165,    2, 0x08 /* Private */,
+      17,    0,  166,    2, 0x08 /* Private */,
+      18,    0,  167,    2, 0x08 /* Private */,
+      19,    0,  168,    2, 0x08 /* Private */,
+      20,    0,  169,    2, 0x08 /* Private */,
+      21,    0,  170,    2, 0x08 /* Private */,
+      22,    0,  171,    2, 0x08 /* Private */,
+      23,    0,  172,    2, 0x08 /* Private */,
+      24,    0,  173,    2, 0x08 /* Private */,
+      25,    0,  174,    2, 0x08 /* Private */,
+      26,    0,  175,    2, 0x08 /* Private */,
+      27,    0,  176,    2, 0x08 /* Private */,
+      28,    0,  177,    2, 0x08 /* Private */,
+      29,    0,  178,    2, 0x08 /* Private */,
+      30,    0,  179,    2, 0x08 /* Private */,
+
+ // signals: parameters
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,    4,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,   11,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MaxPlot::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MaxPlot *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->windowClosed(); break;
+        case 1: _t->sendMQTTMessage((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 2: _t->Finish_ALL(); break;
+        case 3: _t->finishRead(); break;
+        case 4: _t->Finish_Mqtt(); break;
+        case 5: _t->Finish_Plot(); break;
+        case 6: _t->updatePlot(); break;
+        case 7: _t->onSliderMoved((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 8: _t->onExitButtonClicked(); break;
+        case 9: _t->onStartButtonClicked(); break;
+        case 10: _t->Read_Data_Thread(); break;
+        case 11: _t->Update_Plot_Thread(); break;
+        case 12: _t->Mqtt_Thread(); break;
+        case 13: _t->Get_Mqtt_Message(); break;
+        case 14: _t->onSliderPressed(); break;
+        case 15: _t->onSliderReleased(); break;
+        case 16: _t->Http_Worker_Start(); break;
+        case 17: _t->startTimerIfReady(); break;
+        case 18: _t->stop_Plot_Timer(); break;
+        case 19: _t->stop_Mqtt_Thread(); break;
+        case 20: _t->stop_Read_Thread(); break;
+        case 21: _t->setup_timer(); break;
+        case 22: _t->start_timer(); break;
+        case 23: _t->pause_timer(); break;
+        case 24: _t->onMqttFinished(); break;
+        case 25: _t->delete_timer(); break;
+        case 26: _t->onPlotFinished(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            using _t = void (MaxPlot::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&MaxPlot::windowClosed)) {
+                *result = 0;
+                return;
+            }
+        }
+        {
+            using _t = void (MaxPlot::*)(const QString & );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&MaxPlot::sendMQTTMessage)) {
+                *result = 1;
+                return;
+            }
+        }
+        {
+            using _t = void (MaxPlot::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&MaxPlot::Finish_ALL)) {
+                *result = 2;
+                return;
+            }
+        }
+        {
+            using _t = void (MaxPlot::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&MaxPlot::finishRead)) {
+                *result = 3;
+                return;
+            }
+        }
+        {
+            using _t = void (MaxPlot::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&MaxPlot::Finish_Mqtt)) {
+                *result = 4;
+                return;
+            }
+        }
+        {
+            using _t = void (MaxPlot::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&MaxPlot::Finish_Plot)) {
+                *result = 5;
+                return;
+            }
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject MaxPlot::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_MaxPlot.data,
+    qt_meta_data_MaxPlot,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *MaxPlot::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MaxPlot::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MaxPlot.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MaxPlot::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 27)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 27;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 27)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 27;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void MaxPlot::windowClosed()
+{
+    QMetaObject::activate(this, &staticMetaObject, 0, nullptr);
+}
+
+// SIGNAL 1
+void MaxPlot::sendMQTTMessage(const QString & _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t1))) };
+    QMetaObject::activate(this, &staticMetaObject, 1, _a);
+}
+
+// SIGNAL 2
+void MaxPlot::Finish_ALL()
+{
+    QMetaObject::activate(this, &staticMetaObject, 2, nullptr);
+}
+
+// SIGNAL 3
+void MaxPlot::finishRead()
+{
+    QMetaObject::activate(this, &staticMetaObject, 3, nullptr);
+}
+
+// SIGNAL 4
+void MaxPlot::Finish_Mqtt()
+{
+    QMetaObject::activate(this, &staticMetaObject, 4, nullptr);
+}
+
+// SIGNAL 5
+void MaxPlot::Finish_Plot()
+{
+    QMetaObject::activate(this, &staticMetaObject, 5, nullptr);
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
