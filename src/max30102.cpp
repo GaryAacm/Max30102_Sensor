f794ad2f251e99197469658816b6281fe9e6c7de
小子 <136250709+GaryAacm@users.noreply.github.com>
2025-03-17 12:39:07 +0800

    Update max30102.cpp

--- src/max30102.cpp
+++ src/max30102.cpp
@@ -24,7 +24,8 @@ using namespace std;
 MAX30102::MAX30102(const char *device, uint8_t tcaAddress, uint8_t maxAddress)
     : device(device), tcaAddress(tcaAddress), maxAddress(maxAddress)
 {
-    // Init_Sensor();
+    fd = init_i2c(device, TCA9548A_ADDR);
+    scanf_channel();
 }
 
 MAX30102::~MAX30102()
@@ -34,7 +35,7 @@ MAX30102::~MAX30102()
 void MAX30102::Init_Sensor()
 {
     fd = init_i2c(device, TCA9548A_ADDR);
-    scanf_channel();
+    // scanf_channel();
     Init_channel_sensor();
 }
 
@@ -103,9 +104,10 @@ void MAX30102::scanf_channel()
             continue;
         }
     }
+    cout << "count_channel is:" << count_channel << endl;
     for (int i = 0; i < count_channel; i++)
     {
-        std::cout << "channel : " << enable_channels[i] << std::endl;
+        std::cout << i << "channel : " << enable_channels[i] << std::endl;
     }
 }
 
@@ -183,9 +185,10 @@ void MAX30102::get_data()
 {
     uint32_t red_temp, ir_temp;
 
-    for (int i = 0; i <= count_channel; i++)
+    for (int i = 0; i < count_channel; i++)
     {
         int use_channel = 1 << enable_channels[i];
+        cout << i << "Now is" << enable_channels[i] << endl;
         int check = write(fd, &use_channel, 1);
         if (check != 1)
             continue;
@@ -201,14 +204,12 @@ void MAX30102::get_data()
 
         read_fifo(&red_temp, &ir_temp, *max30102_fd_ptr);
 
-        // printf("Reading data is:%d %d %d",enable_channels[i],red_temp,ir_temp);
-
-        //data.channel_id[i] = enable_channels[i];
+        printf("Reading data is:%d %d %d\n", enable_channels[i], red_temp, ir_temp);
 
         data.redData[enable_channels[i]] = red_temp;
         data.irData[enable_channels[i]] = ir_temp;
     }
-    //cout << "Check_data is" << check_count++ << endl;
+    // cout << "Check_data is" << check_count++ << endl;
 
     emit dataReady(data);
 }
